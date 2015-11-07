4aea2e861c0a79997c754299ecde9963af5dea91
Jorin Weatherston <jorinw@8.linux.csc.uvic.ca>
2015-11-06 20:55:46 -0800

    Clerk in development

--- PQS.c
+++ PQS.c
@@ -4,8 +4,19 @@
 #include <unistd.h>  //intptr_t
 #include <time.h>
 #include <string.h>
+
 //the number of customers
 int num;
+//convar used by customers to signal the clerk
+pthread_cond_t clerkConvar = PTHREAD_COND_INITIALIZER;
+//mutex used to protect the customer attributes array
+pthread_mutex_t custArrayLock;
+//mutex used to signal the clerk is a garbage mutex
+pthread_mutex_t clerkLock;
+//mutex for the customer time decrementation process
+pthread_mutex_t decrementLock;
+//mutex for recieving signals from clerk inside of customer
+pthread_mutex_t custSigLock;
 
 /* create thread argument struct for thr_func() */
 typedef struct _customer{
@@ -13,7 +24,7 @@ typedef struct _customer{
 	int priority;
 	int arrivalTime; 
   int serviceTime;
-  pthread_cond_t convar;
+  pthread_cond_t customerConvar;
   int arrived;
   int running;
   int waiting;
@@ -24,10 +35,25 @@ typedef struct _customer{
 void *customer_function(void *ptr){
 	customer * cPtr = (customer *)ptr;
   //wait until arrival time has passed
-	sleep(cPtr->arrivalTime);
+	usleep(cPtr->arrivalTime*100000);
+  //lock the mutex on attribute reads and writes
+  pthread_mutex_lock(&custArrayLock);
+  //set the arrived state for this customer to 1
+  cPtr->arrived = 1;
+  //set the waiting state for this customer to 1
+  cPtr->waiting = 1;
   //state that the customer has arrived
   printf("customer %2d arrives: arrival time (%d), service time (%d), priority (%2d)\n", cPtr->id, cPtr->arrivalTime, cPtr->serviceTime, cPtr->priority);
-  //signal the clerk that this thread has arrived. Pass the id of the customer.
+  //unlock the customer array lock
+  pthread_mutex_unlock(&custArrayLock);
+  //signal the clerk that this thread has arrived.
+  pthread_cond_signal(&clerkConvar);
+  //wait on this threads convar
+  pthread_cond_wait(&cPtr->customerConvar, &custSigLock);
+  //TEST: print out that we recieved a message from the clerk
+  printf("Thread recieved message from clerk to begin running");
+  //immediately unlock garbage clerkLock
+  pthread_mutex_unlock(&clerkLock);
   //wait for a signal from the clerk saying that its turn has come
   //run until signalled to stop by the clerk or run out of time
     //if signalled to stop, got to above step where we were waiting
@@ -40,27 +66,49 @@ void *customer_function(void *ptr){
 /* clerk function */
 void *clerk_function(void* cA){
   customer * customerArray = (customer *)cA;
-  //TEST: print out all the attributes of the customers
-  int i;
   //customerArray[i].id, 
   //customerArray[i].arrivalTime, 
   //customerArray[i].serviceTime, 
   //customerArray[i].priority);
   
-  //waits (sleep, or convar, or mutex) until a customer signals arrival with id. Most likely a convar.
-  //(A): if there is no currently running customer (running bool) then immediately signal start the newly arrived process
-  //if there is a customer running (as noted by a running bool), then it is the highest priority of waiting and running customers so check if the newly arrived process is higher
-    //if the new customer isn't higher, then add the customer to the waiting list
-      //print out that the customer was blocked
-    //if the new customer is higher, then signal stop to the currently running process and putting it back into the waiting list, and signal start to the new process.
-      //print out that the customer was interupted by a higher priority
-    //if there is a tie of priority give the priority to
-      //the one that arrived first
-      //then to the one with shortest service
-      //then lower id number (it is assumed that id numbers increased linearly and so customers earlier in the file have lower ids)
-  //wait for the running customer to signal completion
-  //set running bool to false;
-  //if another customer arrives, then go back to (A)
+  int processesCompleted = 0;
+  //loop until all customers are finished
+  while(processesCompleted < num){
+   //waits (sleep, or convar, or mutex) until a customer signals arrival. Most likely a convar.
+   pthread_cond_wait(&clerkConvar, &clerkLock);
+   //A customer has arrived so now we need to check its properties
+   //got through all the customer and check for the single one that has arrived
+   int signallingCustomerID = 0;
+   int runningCustomerID = 0;
+   int i;
+   for(i = 0; i < num; i++){
+     if(customerArray[i].running == 1){
+       runningCustomerID = customerArray[i].id;
+     }
+     if(customerArray[i].arrived == 1){
+       signallingCustomerID = customerArray[i].id;
+     }
+   }
+   //case 1: there is no running customer. Run the signalling customer
+   if(!runningCustomerID){
+     
+   }
+    //(A): if there is no currently running customer (running bool) then immediately signal start the newly arrived process
+    //if there is a customer running (as noted by a running bool), then it is the highest priority of waiting and running customers so check if the newly arrived process is higher
+      //if the new customer isn't higher, then add the customer to the waiting list
+        //print out that the customer was blocked
+      //if the new customer is higher, then signal stop to the currently running process and putting it back into the waiting list, and signal start to the new process.
+        //print out that the customer was interupted by a higher priority
+      //if there is a tie of priority give the priority to
+        //the one that arrived first
+        //then to the one with shortest service
+        //then lower id number (it is assumed that id numbers increased linearly and so customers earlier in the file have lower ids)
+    //wait for the running customer to signal completion
+    //set customer running bool to false;
+    //if another customer arrives, then go back to (A)
+  
+   processesCompleted++;
+  }
   //thread kill with wait to make sure that prints happen
   sleep(1);
   pthread_exit(NULL);
@@ -99,8 +147,34 @@ int main(int argc, char* argv[])
   customer cusArray[num]; //customer array
 	pthread_t thrdArray[num + 1]; //thread array with extra for the clerk (this is so we can get main to wait on all threads including clerk)
   
+  //initialize the customer mutex
+  if(pthread_mutex_init(&custArrayLock, NULL) != 0){
+    printf("\nmutex init failed\n");
+    return 1;
+  }
+  
+  //initialize the clerk mutex
+  if(pthread_mutex_init(&clerkLock, NULL) != 0){
+    printf("\nmutex init failed\n");
+    return 1;
+  }
+  
+  //initialize the decrement mutex
+  if(pthread_mutex_init(&decrementLock, NULL) != 0){
+    printf("\nmutex init failed\n");
+    return 1;
+  }
+  
+  //initialize the decrement mutex
+  if(pthread_mutex_init(&custSigLock, NULL) != 0){
+    printf("\nmutex init failed\n");
+    return 1;
+  }
+ 
   //get each line of the input file and create a thread based on it.
   //initialize all of the threads with their properties 
+  //lock the mutex during the init process
+  pthread_mutex_lock(&custArrayLock);
   for (i = 0; fgets(line, sizeof(line), fp); ++i) {  
     char *pChr = strtok(line, ":,");
     int j;
@@ -119,7 +193,7 @@ int main(int argc, char* argv[])
       }
       pChr = strtok(NULL, ":,");
     }
-    cusArray[i].convar = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
+    cusArray[i].customerConvar = (pthread_cond_t)PTHREAD_COND_INITIALIZER;
     cusArray[i].arrived = 0;
     cusArray[i].running = 0;
     cusArray[i].waiting = 0;
@@ -129,7 +203,6 @@ int main(int argc, char* argv[])
       return EXIT_FAILURE;
 	  }
   }
-  
   //close the file
   fclose(fp);
   
@@ -139,7 +212,8 @@ int main(int argc, char* argv[])
       fprintf(stderr, "error: pthread_create, rc: %d\n", rc);
       return EXIT_FAILURE;
   }
-	
+  //unlock the init mutex for custArray
+	pthread_mutex_unlock(&custArrayLock);
 	// block until all threads complete 
   int j;
   for (j = 0; j < num + 1; ++j) {
@@ -149,37 +223,3 @@ int main(int argc, char* argv[])
   //sleep();
 	return EXIT_SUCCESS;
 }
-
-////takes in a line from the file and returns an array of 4 numbers,
-////each of which is an attribute for a thread
-//int * createAttributes(char line[15]){
-//      printf("Line to create attributes from: %s\n", line);
-//      const char colon[2] = ":";
-//      const char comma[2] = ",";
-//      char *idAndRemainder;
-//      char *attrs;
-//      static int attributes[4] = {0,0,0,0};
-//      printf("Hello");
-//      //pull out the id
-//      idAndRemainder = strtok(line, colon);
-//      
-//      //save the id in the attributes array
-//      attributes[0] = atoi(idAndRemainder);
-//      //move past the id
-//      idAndRemainder = strtok(NULL, colon);
-//      
-//      //split the non-id attributes by the comma
-//      attrs = strtok(idAndRemainder, comma);
-//      //save the arrival time in attributes array
-//      attributes[1] = atoi(attrs);
-//      //move to the service time
-//      attrs = strtok(NULL, comma);
-//      //save the service time in attributes array
-//      attributes[2] = atoi(attrs);
-//      //move to the priority
-//      attrs = strtok(NULL, comma);
-//      //save the priority in the attributes array
-//      attributes[3] = atoi(attrs);
-//           
-//      return attributes;
-//}
